fb25c8ff638939821cc1b31f0973e2e5b6f25b2e
Alvaro Cordero <alvarocormin@gmail.com>
2024-05-08 00:23:22 +0200

    Last modify

--- src/main/java/curso/java/tienda/controller/UsuarioController.java
+++ src/main/java/curso/java/tienda/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package curso.java.tienda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import curso.java.tienda.model.UsuarioVO;
 import curso.java.tienda.repository.UsuarioRepository;
@@ -23,7 +25,28 @@ public class UsuarioController {
 		model.addAttribute("clients", usuarioRepository.findById_Rol(1));
 		return "client-list";
 	}
-	
+
+	@GetMapping("/clients/cambiarClave")
+	public String verCambiarClaves() {
+		return "cambiarClave";
+
+	}
+
+	@PostMapping("/clients/cambiarClave/edit")
+	public String CambiarClaves(@RequestParam String nuevaClave, @RequestParam String clave,
+			@RequestParam String claveRepetida) {
+
+		if (!clave.equals(claveRepetida)) {
+			return "cambiarClave";
+		} else {
+			String claveEncriptada = BCrypt.hashpw(nuevaClave, BCrypt.gensalt());
+			usuarioRepository.actualizarClaveUsuario("administrador@gmail.com", claveEncriptada);
+			return "dashboard";
+
+		}
+
+	}
+
 	@GetMapping("/clients/form")
 	public String getEmptyForm(Model model) {
 		model.addAttribute("client", new UsuarioVO());
@@ -40,32 +63,31 @@ public class UsuarioController {
 		}
 
 	}
-	
+
 	@PostMapping("/clients")
 	public String createUser(@ModelAttribute UsuarioVO usuario) {
-		 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-	    if (usuario.getId() != 0) {
-	        usuarioRepository.findById(usuario.getId()).ifPresent(existingUser -> {
-	            existingUser.setNombre(usuario.getNombre());
-	            existingUser.setApellido1(usuario.getApellido1());
-	            existingUser.setApellido2(usuario.getApellido2());
-	            existingUser.setEmail(usuario.getEmail());
-	            existingUser.setId_rol(usuario.getId_rol());
-	            existingUser.setDireccion(usuario.getDireccion());
-	            existingUser.setLocalidad(usuario.getLocalidad());
-	            existingUser.setProvincia(usuario.getProvincia());
-	            existingUser.setTelefono(usuario.getTelefono());
-	            existingUser.setDni(usuario.getDni());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if (usuario.getId() != 0) {
+			usuarioRepository.findById(usuario.getId()).ifPresent(existingUser -> {
+				existingUser.setNombre(usuario.getNombre());
+				existingUser.setApellido1(usuario.getApellido1());
+				existingUser.setApellido2(usuario.getApellido2());
+				existingUser.setEmail(usuario.getEmail());
+				existingUser.setId_rol(usuario.getId_rol());
+				existingUser.setDireccion(usuario.getDireccion());
+				existingUser.setLocalidad(usuario.getLocalidad());
+				existingUser.setProvincia(usuario.getProvincia());
+				existingUser.setTelefono(usuario.getTelefono());
+				existingUser.setDni(usuario.getDni());
 
-	            usuarioRepository.save(existingUser);
-	        });
-	    } else {
-	    	usuario.setId_rol(1);
-	    	usuario.setClave(encoder.encode(usuario.getClave()));
-	        usuarioRepository.save(usuario);
-	    }
-	    return "redirect:/clients";
+				usuarioRepository.save(existingUser);
+			});
+		} else {
+			usuario.setId_rol(1);
+			usuario.setClave(encoder.encode(usuario.getClave()));
+			usuarioRepository.save(usuario);
+		}
+		return "redirect:/clients";
 	}
 
-	
 }
--- src/main/java/curso/java/tienda/repository/UsuarioRepository.java
+++ src/main/java/curso/java/tienda/repository/UsuarioRepository.java
@@ -3,8 +3,10 @@ package curso.java.tienda.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import curso.java.tienda.model.UsuarioVO;
 
@@ -16,7 +18,12 @@ public interface UsuarioRepository extends JpaRepository<UsuarioVO, Integer> {
 
 	@Query("SELECT u FROM UsuarioVO u WHERE u.id_rol = :idRol")
 	List<UsuarioVO> findById_Rol(int idRol);
-	
+
 	@Query("SELECT u FROM UsuarioVO u WHERE u.id_rol = :idRol1 OR u.id_rol = :idRol2")
-    List<UsuarioVO> findByRoles(int idRol1, int idRol2);
+	List<UsuarioVO> findByRoles(int idRol1, int idRol2);
+
+	@Transactional
+	@Modifying
+	@Query("UPDATE UsuarioVO u SET u.clave = :nuevaClave WHERE u.email = :emailUsuario")
+	void actualizarClaveUsuario(String emailUsuario, String nuevaClave);
 }
