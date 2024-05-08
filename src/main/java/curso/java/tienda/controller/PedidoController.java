7455f39b416c4c6fe91f02c71f661517d99276e5
Alvaro Cordero Miñambres <acordero@serbatic.es>
2024-05-08 12:55:07 +0200

    change test7

--- src/main/java/curso/java/tienda/controller/PedidoController.java
+++ src/main/java/curso/java/tienda/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package curso.java.tienda.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import curso.java.tienda.model.CategoriaVO;
+import curso.java.tienda.model.DetallePedidoVO;
 import curso.java.tienda.model.PedidoVO;
+import curso.java.tienda.repository.DetallePedidoRepository;
 import curso.java.tienda.repository.PedidoRepository;
 
 @Controller
@@ -17,6 +21,11 @@ public class PedidoController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private DetallePedidoRepository detallePedidoRepository;
+
+
 
 	@GetMapping("/pedidos")
 	public String findAll(Model model) {
@@ -26,13 +35,28 @@ public class PedidoController {
 
 
 	// Método para cambiar el estado del pedido a "E"
-    @GetMapping("/pedidos/cambiarEstado/{id}")
+	@GetMapping("/pedidos/cambiarEstado/{id}")
     public String cambiarEstadoPedido(@PathVariable Integer id) {
         pedidoRepository.findById(id).ifPresent(existingPedido -> {
             existingPedido.setEstado("E");
+
+            // Recuperar todas las líneas de pedido para este pedido específico
+            List<DetallePedidoVO> detallesPedido = detallePedidoRepository.findByIdPedido(id);
+
+            // Calcular el total sumando el valor total de cada línea de pedido
+            double totalPedido = detallesPedido.stream()
+                    .mapToDouble(detallePedido -> detallePedido.getTotal())
+                    .sum();
+
+            // Asignar el total calculado al pedido existente
+            existingPedido.setTotal(totalPedido);
+            
+            // Guardar los cambios en el pedido
             pedidoRepository.save(existingPedido);
         });
         return "redirect:/pedidos";
     }
+    
+    
 
 }
